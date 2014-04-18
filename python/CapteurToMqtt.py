edfaf0f74627825da5ce6545b7e6352d90f22ade
Yann Blanc <yann.blanc.38144@gmail.com>
2014-04-18 12:39:10 +0100

    Correction script python

--- python/CapteurToMqtt.py
+++ python/CapteurToMqtt.py
@@ -6,11 +6,11 @@ pGazValue='0'
 gazValue='0'
 
 while True:
-f = open ("/sys/bus/iio/devices/iio\:device0/in_voltage0_raw","r")
-cat        /sys/bus/iio/devices/iio\:device0/in_voltage0_raw
-gazValue = f.readline()
-if (gazValue != pGazValue):
-pGazValue = gazValue
-os.system("mosquitto_pub -t capteur -m $gazValue")
-f.close()
-time.sleep(1);
+  f = open ("/sys/bus/iio/devices/iio:device0/in_voltage0_raw","r")
+  gazValue = f.readline()
+  if (gazValue != pGazValue):
+    print "new gazValue : " + gazValue
+    pGazValue = gazValue	    
+    os.system("mosquitto_pub -h localhost -t capteur -m " + gazValue)
+  f.close()
+  time.sleep(1);
